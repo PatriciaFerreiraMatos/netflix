37161913448a1647280692a57edda69747c92515
vai na web <vainawebcpx@gmail.com>
2019-11-11 11:36:14 -0300

    começando a main

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 function pesquisa() {
     document.querySelector('input').style.display = 'flex';
-    document.querySelector('blockinput').style.border = '3px solid #fff'
-
+    document.querySelector('.blockinput').style.border = '0.3px solid #fff';
+    document.querySelector('input').style.display = ('none')
 }
+// else{
+//     document.querySelector('input').style.display = 'flex';
+    
+// }
